97b3ae5f015088e687ed23819bc071977f51d51c
DIALLO Souleymane <72746804+souleymane-diallo@users.noreply.github.com>
2021-02-23 10:48:52 +0100

    mise à jour

---
+++ js/index.js
@@ -0,0 +1,3 @@
+// key API
+const keyAPI = '7adc3d87cd6b452fa2420595f622b8a7';
+const getUrl = `http://newsapi.org/v2/everything?q=tesla&from=2021-01-17&sortBy=publishedAt&apiKey=7adc3d87cd6b452fa2420595f622b8a7`;
